99a792221f75f0fa282b87497930c8989a4df582
Victor Avellar <victor.avellar@sonda.com>
2025-12-18 00:23:20 -0300

    feat: creating http client

---
+++ frontend/src/infra/http-client.ts
@@ -0,0 +1,26 @@
+import axios, {
+  AxiosInstance,
+  AxiosRequestConfig,
+  AxiosResponse
+} from 'axios';
+
+export class HttpClient {
+  private client: AxiosInstance;
+
+  constructor(baseURL: string) {
+    this.client = axios.create({
+      baseURL,
+      timeout: 10000,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
+
+
+
+  async get<T>(url: string, config?: AxiosRequestConfig): Promise<T> {
+    const response: AxiosResponse<T> = await this.client.get(url, config);
+    return response.data;
+  }
+}
