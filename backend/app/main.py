6f5da12e83902e105dc2f2641eefa620c9bf7eaa
Victor Avellar <victor.avellar@sonda.com>
2025-12-18 00:22:37 -0300

    feat: adding middleware to allow any origin

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers.people import router as people_router
 from app.routers.movies import router as movies_router
 
 app = FastAPI(title="People")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"], 
+    allow_headers=["*"], 
+)
 
 app.include_router(people_router)
 app.include_router(movies_router)
